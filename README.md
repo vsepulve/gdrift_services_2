8f091380b5624854cbfbf4c14ceead072ac0b875
vsepulve <vsepulve@dcc.uchile.cl>
2017-12-28 13:10:10 -0300

    Baseline, direct compilation (without Makefile or CMakeLists.txt for the moment).

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gdrift_services_2
 Temporary repository for the proposed version 2 of Gdrift Services.
+
+Build
+-----
+```
+g++ src/test.cpp -o bin/test -I/usr/local/include/gdrift++ -std=c++11 -lgdrift++
+```
---
+++ src/test.cpp
@@ -0,0 +1,15 @@
+
+#include <Simulator.h>
+#include <Sample.h>
+#include <NanoTimer.h>
+
+int main(int argc, char** argv){
+	
+	NanoTimer timer;
+	
+	cout<<"Inicio \n";
+	
+	cout<<"Fin ("<<timer.getMilisec()<<" ms)\n";
+	
+}
+
